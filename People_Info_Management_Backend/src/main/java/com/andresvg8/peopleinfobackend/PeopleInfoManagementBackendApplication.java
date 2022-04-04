ffd41e8a4162489f8425dc7f897f1c0e6bb7741d
Andres Villalba Gaviria <66088651+andresvg8@users.noreply.github.com>
2022-04-04 14:06:28 -0500

    Initial commit

---
+++ People_Info_Management_Backend/src/main/java/com/andresvg8/peopleinfobackend/PeopleInfoManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.andresvg8.peopleinfobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleInfoManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleInfoManagementBackendApplication.class, args);
+	}
+
+}
---
+++ People_Info_Management_Backend/src/test/java/com/andresvg8/peopleinfobackend/PeopleInfoManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresvg8.peopleinfobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleInfoManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
